d48421f7f8f68831cf677f5e01e10dd18d142a27
JoharryJC <41237769+JoharryJC@users.noreply.github.com>
2021-03-08 21:43:41 +0100

    Dependency Injection with Spring and Primary Beans

--- src/main/java/guru/springframework/sfgdisp5/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdisp5/controllers/MyController.java
@@ -2,12 +2,24 @@ package guru.springframework.sfgdisp5.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.sfgdisp5.services.GreetingService;
+
 @Controller
 public class MyController {
 
+	private final GreetingService greetingService; 	
+	
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+
+
+
 	public String sayHello() {
-		System.out.println("Hello World!!!");
-		return "Hi amigo"; 
+		//System.out.println("Hello World!!!");
+		//return "Hi amigo";
+		return greetingService.sayGreeting(); 
 		
 	}
 }
---
+++ src/main/java/guru/springframework/sfgdisp5/services/PrimaryGreetingService.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgdisp5.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService {
+
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return "Hello World - PrimaryGreetingService  --- @Primary Bean";
+	}
+
+	
+}
